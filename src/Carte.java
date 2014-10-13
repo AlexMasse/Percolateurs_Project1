dddc5d312f015546a047a2f6ed0308ea304149b4
Dom <do@not.mail.me>
2014-10-13 16:06:29 -0400

    commentaires ajoutées teste de réalisation ajoutés retirer le system exit du frame affiche le frame des le depart

--- src/Carte.java
+++ src/Carte.java
@@ -1,37 +1,72 @@
 /**
- * Created by ultimatum on 2014-10-02.
+ * INF6150
+ *
+ * Represente une carte a jouer possedant une couleur et une valeur determine
+ * par sa position dans un paquet de cartes. Il est laisse a l'utilisateur de
+ * decider comment utiliser ces valeurs. Une carte est immuable une fois instanciee.
+ *
+ * Creation      : 2014/10/07
+ * @author Percolateurs
+ * @version 1.0
+ *
  */
+
 public class Carte {
 
     private final int couleur;
     private final int valeur;
     private final int positionPaquet;
 
-    //doit être une carte valide
+    /**
+     * Intancie une carte en lui fournissant la couleurm la valeur et sa position
+     * dans le paquet.
+     *
+     * @param couleur la couleur de la carte
+     * @param valeur la valeur de la carte
+     * @param positionPaquet la position de la carte dans le paquet
+     */
     public Carte (int couleur, int valeur, int positionPaquet) {
         this.couleur = couleur;
         this.valeur  = valeur;
         this.positionPaquet = positionPaquet;
     }
+
+    /**
+     * Intancie une carte en lui fournissant sa position
+     * dans le paquet.
+     *
+     * @param positionPaquet La position dans le paquet
+     */
     public Carte (int positionPaquet) {
         this.couleur = PaquetDeCartes.couleur(positionPaquet);
         this.valeur  = PaquetDeCartes.valeur(positionPaquet);
         this.positionPaquet = positionPaquet;
     }
 
-
+    /**
+     * Retourne la valeur de la carte
+     * @return La valeur de la carte
+     */
     public int getValeur() {
 
         return this.valeur;
     }
+
+    /**
+     * Retourne la couleur de la carte
+     * @return la couleur de la carte
+     */
     public int getCouleur() {
 
         return this.couleur;
     }
+
+    /**
+     * Retourne la position de la carte dans le paquet
+     * @return la position de la carte dans le paquet
+     */
     public int getPositionPaquet() {
 
         return this.positionPaquet;
     }
-
-
 }
--- src/VueJeuxPari.java
+++ src/VueJeuxPari.java
@@ -1,12 +1,20 @@
 /**
- * Created by ultimatum on 2014-10-07.
+ * INF6150
+ *
+ * Gere l'affichage des cartes du jeu
+ *
+ * Creation      : 2014/10/07
+ * @author Percolateurs
+ * @version 1.0
+ *
  */
 import javax.swing.*;
 
 public class VueJeuxPari extends JPanel {
 
+    //representation unicode du sigle de coeur, carreau, trefle et pique
     final private static String[] COULEURS = {"\u2665", "\u2666", "\u2663", "\u2660"};
-
+    //couleur de l'arriere plan du jeu (vert "casino")
     final private static java.awt.Color COULEUR_TAPIS =  new java.awt.Color(34,129,34);
 
     public VueJeuxPari(int hauteur, int largeur) {
@@ -14,21 +22,40 @@ public class VueJeuxPari extends JPanel {
         this.setPreferredSize(new java.awt.Dimension(hauteur,largeur));
 
     }
+
+    /**
+     * Retire toutes les cartes de l'affichage
+     */
     public void vider() {
         this.removeAll();
     }
+
+    /**
+     * Affiche les cartes contenues dans un tableau a la console
+     * et les ajoutes dans l'affichage complexe.
+     *
+     * @param cartes le tableau de cartes
+     * @param nombre le nombre de cartes dans le tableau
+     */
     public void afficher(Carte[] cartes, int nombre) {
         this.removeAll();
-        for (int i = 0; i < nombre; ++i) {
-            this.add(new ImageCarte(cartes[i]));
+        for ( int i = 0; i < nombre; ++i ) {
+            this.add( new ImageCarte(cartes[i]) );
             afficherConsole(cartes[i]);
         }
         repaint();
     }
 
+    /**
+     * Affiche une carte a la console
+     * @param carte la carte a afficher
+     */
     private void afficherConsole(Carte carte) {
+        //cadre superieur
         String dessus = ".---.";
+        //cadre inferieur
         String dessous = "'---'";
+        //convertion des cartes > 10 en leur valeur literaire
         String valeur;
 
         switch (carte.getValeur()) {
